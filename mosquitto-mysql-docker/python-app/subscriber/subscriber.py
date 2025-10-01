3914b3d290dc68a1ff58af532161641ab73cf648
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-10-01 14:05:59 -0400

    restart conf mysql

--- mosquitto-mysql-docker/python-app/subscriber/subscriber.py
+++ mosquitto-mysql-docker/python-app/subscriber/subscriber.py
@@ -1,3 +1,56 @@
+import mysql.connector
+
+from flask import Flask
+
+import os
+
+app = Flask(__name__)
+
+# Get database credentials from environment variables
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+
+
+def connect_to_mysql():
+    try:
+        return mysql.connector.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DATABASE,
+            port=3306,
+        )
+    except mysql.connector.Error as err:
+        print(f"Error connecting to MySQL: {err}")
+        return None
+
+
+@app.route("/")
+def index():
+    return "Hello from Flask!"
+
+
+@app.route("/databases")
+def list_databases():
+    try:
+        db_connection = connect_to_mysql()
+        with db_connection.cursor() as cursor:
+            cursor.execute("SHOW DATABASES;")
+            databases = cursor.fetchall()
+        db_connection.close()
+        return {"databases": [db["Database"] for db in databases]}
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+    
+    
+
+"""
 import paho.mqtt.client as mqtt
 
 import mysql.connector
@@ -6,8 +59,14 @@ import json
 import os
 import time
 
-broker_host = os.getenv("MQTT_BROKER_HOST", "mqtt-broker")
-broker_port = int(os.getenv("MQTT_BROKER_PORT", 1883))
+MQTT_BROKER_HOST = os.getenv("MQTT_BROKER_HOST")
+MQTT_BROKER_PORT = os.getenv("MQTT_BROKER_PORT")
+
+# Get database credentials from environment variables
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 
 
 def handle_telemetry(client, userdata, message):
@@ -31,24 +90,41 @@ def on_message(client, userdata, message):
     print(f"Received message: {message.payload.decode()} on topic {message.topic}")
 
 
-mydb = mysql.connector.connect(
-    host="mysql-db", user="root", password="123456", port=3306, database="heartbeat_monitor"
-)
+def connect_to_mysql():
+    try:
+        return mysql.connector.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+            database=MYSQL_DATABASE,
+            port=3306,
+        )
+    except mysql.connector.Error as err:
+        print(f"Error connecting to MySQL: {err}")
+        return None
 
-# Create a cursor object
-mycursor = mydb.cursor()
+
+try:
+    db_connection = connect_to_mysql()
+    with db_connection.cursor() as cursor:
+        cursor.execute("SHOW DATABASES;")
+        databases = cursor.fetchall()
+    db_connection.close()
+    print({"databases": [db["Database"] for db in databases]})
+except Exception as e:
+    print({"error": str(e)}, 500)
 
 # Execute the CREATE DATABASE IF NOT EXISTS statement
-#sql_query = "CREATE DATABASE IF NOT EXISTS heartbeat_monitor"
-#mycursor.execute(sql_query)
-#mydb.database = "heartbeat_monitor"
+# sql_query = "CREATE DATABASE IF NOT EXISTS heartbeat_monitor"
+# mycursor.execute(sql_query)
+# mydb.database = "heartbeat_monitor"
 #
-#mycursor.execute(
+# mycursor.execute(
 #    "CREATE TABLE IF NOT EXISTS heartbeat_records (datetime VARCHAR(255), heart_rate INT)"
-#)
+# )
 # mycursor.execute("DROP TABLE IF EXISTS iot_devices")
-#mycursor.execute("SHOW TABLES")
-#for x in mycursor:
+# mycursor.execute("SHOW TABLES")
+# for x in mycursor:
 #    print(x)
 #    mycursor.execute("SHOW COLUMNS FROM heartbeat_records")
 #    for col in mycursor:
@@ -58,9 +134,10 @@ client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(broker_host, broker_port, 60)
+client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
 
 client.loop_start()
 
 while True:
     time.sleep(5)
+"""
--- mysql-docker/mysql-init-scripts:/docker-entrypoint-initdb.d/database.sql
+++ mysql-docker/mysql-init-scripts:/docker-entrypoint-initdb.d/database.sql
@@ -1,10 +1,8 @@
 CREATE DATABASE IF NOT EXISTS heartbeat_monitor;
+USE heartbeat_monitor;
 
 GRANT ALL PRIVILEGES ON heartbeat_monitor.* TO 'root'@'%' IDENTIFIED BY '123456';
 GRANT ALL PRIVILEGES ON heartbeat_monitor.* TO 'raphaelzh'@'%' IDENTIFIED BY '123456';
 FLUSH PRIVILEGES;
 
-USE heartbeat_monitor;
-GRANT ALL PRIVILEGES ON heartbeat_monitor.* TO 'root'@'%' IDENTIFIED BY '123456';
-FLUSH PRIVILEGES;
 CREATE TABLE IF NOT EXISTS heartbeat_records (datetime VARCHAR(255), heart_rate INT);
