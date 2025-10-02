7597d1929f887e299e797c8ea9a7d060f995d292
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-10-02 18:36:19 -0400

    basic pub/sub

---
+++ mosquitto-docker/python-app/subscriber/subscriber.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+import json
+import os
+import time
+
+broker_host = os.getenv("MQTT_BROKER_HOST", "mqtt-broker")
+broker_port = int(os.getenv("MQTT_BROKER_PORT", 1883))
+
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    client.subscribe("test/topic")
+
+
+def on_message(client, userdata, msg):
+    print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
+
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(broker_host, broker_port, 60)
+
+client.loop_start()
+
+while True:
+    time.sleep(5)
