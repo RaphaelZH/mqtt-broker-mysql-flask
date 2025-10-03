e926e9df4c4a20301c7154be1e4632a5515182b5
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-10-03 12:10:29 -0400

    mosquitto-mysql-docker

---
+++ mosquitto-mysql-docker/python-app/publisher/publisher.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+import json
+import os
+import time
+
+broker_host = os.getenv("MQTT_BROKER_HOST", "mqtt-broker")
+broker_port = int(os.getenv("MQTT_BROKER_PORT", 1883))
+
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    client.subscribe("test/topic")
+
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+
+client.connect(broker_host, broker_port, 60)
+
+client.loop_start()
+
+while True:
+    client.publish("test/topic", "Hello from Python!")
+    time.sleep(5)
