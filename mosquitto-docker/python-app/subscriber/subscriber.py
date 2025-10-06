760cfda1a16a4438974c16cfd7f69ac1b43e04c2
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-10-05 23:39:20 -0400

    updates

--- mosquitto-docker/python-app/subscriber/subscriber.py
+++ mosquitto-docker/python-app/subscriber/subscriber.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+
 import json
 import os
 import time
@@ -7,13 +8,19 @@ broker_host = os.getenv("MQTT_BROKER_HOST", "mqtt-broker")
 broker_port = int(os.getenv("MQTT_BROKER_PORT", 1883))
 
 
+def handle_telemetry(client, userdata, message):
+    payload = json.loads(message.payload.decode())
+    print("Message received:", payload)
+
+
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    client.subscribe("test/topic")
+    client.subscribe("heart_rate/topic")
+    client.on_message = handle_telemetry
 
 
-def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
+def on_message(client, userdata, message):
+    print(f"Received message: {message.payload.decode()} on topic {message.topic}")
 
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
--- mosquitto-mysql-docker/python-app/subscriber/subscriber.py
+++ mosquitto-mysql-docker/python-app/subscriber/subscriber.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
 
+import mysql.connector
+
 import json
 import os
 import time
@@ -8,14 +10,49 @@ broker_host = os.getenv("MQTT_BROKER_HOST", "mqtt-broker")
 broker_port = int(os.getenv("MQTT_BROKER_PORT", 1883))
 
 
+def handle_telemetry(client, userdata, message):
+    payload = json.loads(message.payload.decode())
+    print("Message received:", payload)
+
+    sql = "INSERT INTO heartbeat_records (datetime, heart_rate) VALUES (%s, %s)"
+    val = (payload["datetime"], payload["heart_rate"])
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+
+
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     client.subscribe("heart_rate/topic")
+    client.on_message = handle_telemetry
 
 
 def on_message(client, userdata, message):
     print(f"Received message: {message.payload.decode()} on topic {message.topic}")
 
+
+mydb = mysql.connector.connect(
+    host="my-mysql-server", user="root", password="123456", port=13306
+)
+
+# Create a cursor object
+mycursor = mydb.cursor()
+
+# Execute the CREATE DATABASE IF NOT EXISTS statement
+sql_query = "CREATE DATABASE IF NOT EXISTS heartbeat_monitor"
+mycursor.execute(sql_query)
+mydb.database = "heartbeat_monitor"
+
+mycursor.execute(
+    "CREATE TABLE IF NOT EXISTS heartbeat_records (datetime VARCHAR(255), heart_rate INT)"
+)
+# mycursor.execute("DROP TABLE IF EXISTS iot_devices")
+mycursor.execute("SHOW TABLES")
+for x in mycursor:
+    print(x)
+    mycursor.execute("SHOW COLUMNS FROM heartbeat_records")
+    for col in mycursor:
+        print("Column:", col)
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
